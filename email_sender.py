dfbeb20fd3ccccdf78b49ea1d1302a5b6e245072
Daniel Sanches <danielsanches6301@gmail.com>
2023-02-12 21:12:23 -0300

    novos ajustes

--- email_sender.py
+++ email_sender.py
@@ -1,6 +1,12 @@
 import smtplib
 from email.mime.text import MIMEText
 from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE
+from email import encoders
+import time
 
 class EmailSender:
     def __init__(self, usuario, senha):
@@ -12,11 +18,21 @@ class EmailSender:
         self.server.login(usuario, senha)
 
     
-    def enviar_email(self, destinatario, assunto, conteudo):
-        mensagem = MIMEText(conteudo, "html")
+    def enviar_email(self, destinatario, assunto, conteudo, arquivo=None):
+        mensagem = MIMEMultipart("alternative")
         mensagem["Subject"] = assunto
         mensagem["From"] = self.usuario
         mensagem["To"] = destinatario
+
+        part_html = MIMEText(conteudo, 'html')
+
+        mensagem.attach(part_html)
+        if arquivo:
+            part_anexo = MIMEBase("application", "octet-stream")
+            part_anexo.set_payload(open(arquivo, "rb").read())
+            encoders.encode_base64(part_anexo)
+            part_anexo.add_header('Content-Disposition', 'attachment', filename=arquivo)
+            mensagem.attach(part_anexo)
         try:
             self.server.sendmail(self.usuario, destinatario, mensagem.as_string())
             now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -24,4 +40,7 @@ class EmailSender:
             return True
         except Exception as ex:
             print("Erro ao enviar email: {}".format(ex))
+            if "please run connect" in ex:
+                print("Erro 'run connect'. Aguardando 60 segundos...")
+                time.sleep(60)
             return False
--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import pickle
-
 from database import *
 from email_sender import *
 from crawler import *
 import time
 import threading
 
+sender = EmailSender("danielsanches6301@gmail.com", "bpzulbhikogwtons")
 
-# sender = EmailSender("", "")
-
-def enviar_email(email):
+def enviar_email(email, assunto, conteudo, arquivo=None):
     if not email_existe(email):
-        email_enviado = sender.enviar_email(email, "TESTE", "<h1>Email teste</h1>")
+        email_enviado = sender.enviar_email(email, assunto, conteudo, arquivo)
         if email_enviado:
             salvar_email(email)
     else:
@@ -32,11 +30,13 @@ def enviar_email(email):
 
 
 def ler_emails_arquivo():
+    lines = []
     str_arquivo = ""
     with open("emails.txt", "r") as f:
-        str_arquivo = f.read()
+        for line in f:
+            lines.append(line.strip())
 
-    return str_arquivo
+    return lines
 
 def email_existe_arquivo(email):
     emails = ler_emails_arquivo()
@@ -50,6 +50,11 @@ def salvar_email_arquivo(email):
             f.write("{}\n".format(email))
         else:
             print("{} já salvo anteriormente".format(email))
+
+
+def carregar_arquivo(path):
+    with open(path, "rb") as file:
+        return file.read()
 
 
 def executar_crawler(alvo, nivel=2):
@@ -72,8 +77,50 @@ def executar_crawler(alvo, nivel=2):
         print(ex)
 
 
-alvos = encontrar_novas_urls(url)
 
-for i, alvo in enumerate(alvos):
-    print("Buscando emails do alvo {} - {} de {}".format(alvo, i, len(alvos)))
-    executar_crawler(alvo, 3)
+emails = ler_emails_arquivo()
+
+assunto = "Candidato para oportunidade de Desenvolvedor Full-Stack"
+conteudo = """<p>Olá,
+
+Meu nome é Daniel e sou um Desenvolvedor FullStack altamente capacitado, com forte conhecimento em .NET e na construção de páginas web com HTML, CSS e JavaScript.
+
+Deve estar se perguntando como consegui seu endereço de e-mail. Construí uma aplicação em Python que extrai e-mails de sites de vagas de emprego e provavelmente aplicação encontrou o seu em alguma página Web. Queria mostrar que tenho um alto conhecimento técnico e que posso ser um grande valor para uma empresa.</p>
+
+<p>Vim de uma família humilde e aprendi a programar sozinho. Desde cedo sempre tive curiosidade sobre como os sistemas funcionavam e adoro estudar e aprender coisas novas. No meu trabalho atual, trabalho com desenvolvimento e manutenção de aplicações web usando .NET e Vue.js, mas tenho experiência também com outras tecnologias como Angular e React em meus estudos.
+
+Além disso, sou habilidoso em Cloud Computing e tenho duas máquinas na Oracle onde hospedo meus projetos, incluindo meu portfólio que pode ser acessado em https://danielsanchesdev.com.br. Com minha habilidade e experiência, estou confiante de que posso resolver problemas complexos de forma eficiente e contribuir para o sucesso da empresa.</p>
+
+<p>Se você é um recrutador, por favor não deixe de me contatar para que possamos conversar e eu possa mostrar mais sobre minhas habilidades e como posso ser um valor para sua equipe. Caso não seja, peço perdão pelo incômodo e por favor ignore esse e-mail.</p>
+
+<p>Anexei meu currículo para referência.</p>
+
+<p>Agradeço pela sua atenção e espero ouvir de você em breve.</p>
+
+<p>Att,</p>
+<p>Daniel Sanches</p>"""
+
+
+
+def run():
+    for (i, email) in enumerate(emails):
+        print("Enviando email para {} - {} enviado(s) em um total de {}".format(email, i+1, len(emails)))
+        enviar_email(email, assunto, conteudo, "cv_daniel-pereira-sanches.pdf")
+
+
+run()
+# THREADS = []
+
+# for i in range(100):
+#     t = threading.Thread(target=run)
+#     THREADS.append(t)
+#     t.start()
+
+# for t in threads:
+#     t.join()
+# alvos = encontrar_novas_urls(url)
+
+# for i, alvo in enumerate(alvos):
+#     print("Buscando emails do alvo {} - {} de {}".format(alvo, i, len(alvos)))
+#     executar_crawler(alvo, 3)
+
